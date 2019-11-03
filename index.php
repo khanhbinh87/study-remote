c978fc2a16447b2ecb0e0622209580292d79f17a
Tran Ngoc Khanh Binh <tnkb@MacBook-Pro-cua-Tran.local>
2019-11-03 16:21:23 +0700

    update

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+    require_once('index.html');
+    require_once('post-detail.html');
+    require_once('post-list.html');
+    require_once('product-detail.html');
+    require_once('product-list.html');
+    require_once('contact.html');
+?>
